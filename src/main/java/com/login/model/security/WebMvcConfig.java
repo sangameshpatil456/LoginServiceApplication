040cf5d7b8b4e6324c36888e7d5caf670d743590
sangameshpatil456 <sangmesh456@gmail.com>
2021-05-22 17:04:42 +0530

    Pom Change

--- src/main/java/com/login/model/security/WebMvcConfig.java
+++ src/main/java/com/login/model/security/WebMvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -19,11 +20,21 @@ public class WebMvcConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
-				.authorizeRequests().antMatchers(HttpMethod.POST, "/auth/login/").permitAll()
+				.authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/auth/login/").permitAll()
+                .antMatchers(HttpMethod.GET, "/auth/getPublicKey").permitAll()
 				.antMatchers(HttpMethod.POST, "/user/save").permitAll()
-			//	.antMatchers(HttpMethod.GET, "/swagger-ui/").permitAll()
-				.antMatchers("/swagger-ui").permitAll()
 				.anyRequest().authenticated().and().exceptionHandling().accessDeniedPage("/accessdenied");
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/v2/api-docs",
+				"/configuration/ui",
+				"/swagger-resources/**",
+				"/configuration/security",
+				"/swagger-ui.html",
+				"/webjars/**");
 	}
 
 }
